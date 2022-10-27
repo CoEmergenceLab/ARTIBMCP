8b9c6068df7756bd6cd219d78d7e3177dc262c8f
Carlos Castellanos <carloscastellanossf@gmail.com>
2022-10-27 12:55:41 +0200

    Update run.py

--- luciferase/run.py
+++ luciferase/run.py
@@ -14,6 +14,7 @@
 
 # -*- coding: utf-8 -*-
 import argparse
+import random
 import sys
 import os
 import time
@@ -23,10 +24,11 @@ import cv2
 import numpy as np
 import gphoto2 as gp
 import pickle
+
 # Importing the model and methods for transfer learning
-from keras.applications.vgg16 import VGG16 
+from keras.applications.vgg16 import VGG16
 from keras.models import Model
-from keras.applications.vgg16 import preprocess_input 
+from keras.applications.vgg16 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
 from pythonosc import udp_client, osc_message_builder, osc_bundle_builder
@@ -180,22 +182,23 @@ def getGreenPercentage(img):
 
     return finalPercent
 
+
 def extract_features(target_file):
     """
     This function preprocesses the input target image
     and loades the VGG 16 model to extract features.
 
-    return: Extracted features with dimenions 4096 
+    return: Extracted features with dimenions 4096
     """
     # DL model for transfer learning
     model = VGG16()
-    model = Model(inputs = model.inputs, outputs = model.layers[-2].output)
+    model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
     # load the image as a 224x224 array
-    img = load_img(target_file, target_size=(224,224))
+    img = load_img(target_file, target_size=(224, 224))
     # convert from image to numpy array
     img = img_to_array(img)
     # reshape the data for the model reshape(num_of_samples, dim 1, dim 2, channels)
-    reshaped_img = img.reshape(1,224,224,3) 
+    reshaped_img = img.reshape(1, 224, 224, 3)
     # prepare image for model
     imgx = preprocess_input(reshaped_img)
     # get the feature vector
@@ -209,13 +212,13 @@ def preprocess_input_img_for_kmeans(input_target_img_path):
     and extract the features from the target input image and reduces
     the high dimensional features (4096) to 50.
 
-    return: Reduced features with dimensions 50 
+    return: Reduced features with dimensions 50
     """
     # Extracting the features from the target input image
     img_feat = extract_features(input_target_img_path)
-    # Loading PCA pkl file 
-    pca_pkl = 'kmeans_luciferase.pkl'
-    pca = pickle.load(open(pca_pkl, 'rb'))
+    # Loading PCA pkl file
+    pca_pkl = "kmeans_luciferase.pkl"
+    pca = pickle.load(open(pca_pkl, "rb"))
     # Reducing high dimensionality
     reduced_feat = pca.transform(img_feat)
     return reduced_feat
@@ -293,23 +296,72 @@ def main():
 
                 # ==== Load ML model and perform inference ==== #
                 # (make sure image is resized/cropped correctly for the model, e.g. 224x224 for VGG16)
-                model_pkl_file = 'kmeans_luciferase.pkl'
+                model_pkl_file = "kmeans_luciferase.pkl"
                 # Loading model
-                kmeans_model = pickle.load(
-                    open(model_pkl_file, 'rb')
-                    )
+                kmeans_model = pickle.load(open(model_pkl_file, "rb"))
                 # Preprocessing target input image
-                preprocessed_feat = preprocess_input_img_for_kmeans(
-                    img
-                    )
+                preprocessed_feat = preprocess_input_img_for_kmeans(img)
                 # Generate prediction cluster ID
-                cluster_id_prediction = kmeans_model.predict(
-                    preprocessed_feat
-                    )
-            
+                cluster_id_prediction = kmeans_model.predict(preprocessed_feat)
+
                 # then generate a response
                 # Dict with key as input target image and value as predicted cluster ID
-                ml_bundle_dict = {img: cluster_id_prediction[0]}  # for OSC bundle for ml response
+                ml_bundle_dict = {
+                    img: cluster_id_prediction[0]
+                }  # for OSC bundle for ml response
+
+                cluster_distance = ""
+
+                ml_bundle_dict = {
+                    "cluster": {
+                        "address": OSC_ADDRESSES[10],
+                        "arguments": [
+                            [cluster_id_prediction[0], "i"],
+                            [cluster_distance, "f"],
+                        ],
+                    },
+                    "buffers": {
+                        "address": OSC_ADDRESSES[2],
+                        "arguments": [
+                            [random.randint(1, 17), "i"],
+                            [random.randint(1, 17), "i"],
+                        ],
+                    },
+                    "pitch": {
+                        "address": OSC_ADDRESSES[3],
+                        "arguments": [[random.random(), "f"]],
+                    },
+                    "xpos": {
+                        "address": OSC_ADDRESSES[4],
+                        "arguments": [[random.random(), "f"], [random.random(), "f"]],
+                    },
+                    "ypos": {
+                        "address": OSC_ADDRESSES[5],
+                        "arguments": [[random.random(), "f"], [random.random(), "f"]],
+                    },
+                    "chopper": {
+                        address: OSC_ADDRESSES[6],
+                        "arguments": [[random.random(), "f"], [random.random(), "f"]],
+                    },
+                    "water": {
+                        "address": OSC_ADDRESSES[7],
+                        "arguments": [
+                            [random.random(), "f"],
+                            [random.randint(0, 3), "i"],
+                        ],
+                    },
+                    "peg": {
+                        "address": OSC_ADDRESSES[8],
+                        "arguments": [
+                            [random.random(), "f"],
+                            [random.randint(0, 3), "i"],
+                        ],
+                    },
+                    "aba": {
+                        "address": OSC_ADDRESSES[9],
+                        "arguments": [[random.random(), "f"]],
+                    },
+                }
 
                 # ==== Perform contour detection & analysis ==== #
                 # resize image for Syphon
